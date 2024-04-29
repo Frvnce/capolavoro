8819b435312ddb7ff0e4a715569b2f730fa24ef0
Francesco <114349231+Frvnce@users.noreply.github.com>
2024-04-29 22:24:39 +0200

    ProdottoFinale
    daje roma

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-window.onload = function(){
-  document.getElementById("messaggio").textContent = "La vita è bellissima!"; 
-};
+window.onload = function() {
+    leggiFile("file.txt", function(error, frasiArray) {
+      if (error) {
+        console.error("Errore durante la lettura del file:", error);
+      } else {
+        // Genera un indice casuale nell'intervallo da 0 alla lunghezza dell'array di frasi
+        const indiceCasuale = Math.floor(Math.random() * frasiArray.length);
+        // Prendi la frase corrispondente all'indice casuale
+        const fraseCasuale = frasiArray[indiceCasuale];
+        document.getElementById("messaggi").textContent = fraseCasuale;
+      }
+    });
+  };
+  
+  function leggiFile(nomeFile, callback) {
+    fetch(nomeFile)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Errore durante il caricamento del file");
+        }
+        return response.text();
+      })
+      .then(text => {
+        // Split del testo in un array di frasi
+        const frasiArray = text.split('\n').filter(Boolean);
+        callback(null, frasiArray);
+      })
+      .catch(error => {
+        callback(error, null);
+      });
+  }
+  
